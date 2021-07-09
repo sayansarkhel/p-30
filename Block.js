4fe91281c81472c5954a083dca3a34e461380a5b
sayansarkhel <76253744+sayansarkhel@users.noreply.github.com>
2021-07-09 19:39:30 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -1,33 +1,32 @@
 class Block{
-  constructor(x, y, width, height) {
-      var options = {
-          'restitution':0.4,
-          'friction':0.1
+    constructor(x, y, width, height) {
+        var options = {
+            restitution :0.4,
+            friction :0.0,
+        }
+        this.visiblity = 225;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image= loadImage("block.png")
+        World.add(world, this.body);
+        
       }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-      this.visibility = 225;
-      
-      World.add(world, this.body);
+     
+      display(){
+        console.log(this.body.speed);
+        var pos= this.body.position;
+        if(this.body.speed <3){
+          imageMode(CENTER);
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.visiblity = this.visiblity - 5;
+        tint(255,this.visiblity);
+        image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+        pop();
+      }
     }
-    display(){
-      console.log(this.body.speed);
-      if(this.body.speed <3){
-      var angle = this.body.angle;
-      var pos= this.body.position;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      rect(0,0,this.width, this.height);
-      pop();
-    }else{
-      World.remove(world, this.body);
-      push();
-      this.visibility = this.visibility -5;
-      pop();
-      
-    }
-  }
 }
