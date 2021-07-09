3825dea9f4f53161480456eb0f3668ba8cf28c7a
sayansarkhel <76253744+sayansarkhel@users.noreply.github.com>
2021-07-09 19:41:12 +0530

    Update Block2.js

--- Block2.js
+++ Block2.js
@@ -1,25 +1,32 @@
-class Block2{
+class Block{
     constructor(x, y, width, height) {
         var options = {
-            'restitution':0.4,
-            'friction':0.1
-           
+            restitution :0.4,
+            friction :0.0,
         }
+        this.visiblity = 225;
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.visibility=255
+        this.image= loadImage("block.png")
         World.add(world, this.body);
-
-      }
-      display(){
-        push();
-        //translate(this.body.position.x, this.body.position.y);
-        var pos =this.body.position;
-        rectMode(CENTER);
-        fill(rgb(240,128,128));
-        rect(pos.x, pos.y, this.width, this.height);
         
+      }
+     
+      display(){
+        console.log(this.body.speed);
+        var pos= this.body.position;
+        if(this.body.speed <3){
+          imageMode(CENTER);
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.visiblity = this.visiblity - 5;
+        tint(255,this.visiblity);
+        image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
         pop();
       }
-  }
+    }
+}
